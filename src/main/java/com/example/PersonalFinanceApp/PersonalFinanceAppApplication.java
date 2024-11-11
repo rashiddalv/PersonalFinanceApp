6aabb84f2ffb88036f81a424446f8c329c9d29bb
rashiddalv <rashiddalv@gmail.com>
2024-11-11 20:47:27 +0400

    Initializing

---
+++ src/main/java/com/example/PersonalFinanceApp/PersonalFinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.PersonalFinanceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PersonalFinanceApp/PersonalFinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PersonalFinanceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
